5ff2e97c54add7a48e84b182d4fc1fef21e3eceb
danielr <daniel.rodak@untitledkingdom.com>
2019-11-21 17:38:27 +0100

    copied buildSrc from the my another project

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,50 @@
+object Versions {
+    const val kotlin = "1.3.31"
+
+    /** Android versions */
+    const val build_tools_version = "29.0.1"
+    const val compile_sdk_version = 29
+    const val min_sdk_version = 23
+    const val target_sdk_version = 29
+    const val android_gradle_plugin = "3.5.0-rc01"
+
+    /** Libraries */
+    const val androidx = "1.0.2"
+    const val constraint_layout = "1.1.3"
+
+    /** Unit tests */
+    const val junit = "4.12"
+
+    /** Instrumentation tests */
+    const val androidx_junit = "1.1.1"
+    const val androidx_espresso = "3.2.0"
+
+    /** Rx */
+    const val rxJava2 = "2.2.10"
+    const val rxJava2Android = "2.1.1"
+    const val rxBinding = "2.1.1"
+}
+
+object Dependencies {
+    /** Project dependencies */
+    const val android_gradle_plugin = "com.android.tools.build:gradle:${Versions.android_gradle_plugin}"
+    const val kotlin_gradle_plugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
+
+    /** App dependencies */
+    const val kotlin_stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}"
+    const val androidx_appcompat = "androidx.appcompat:appcompat:${Versions.androidx}"
+    const val androidx_core_ktx = "androidx.core:core-ktx:${Versions.androidx}"
+    const val constraint_layout = "androidx.constraintlayout:constraintlayout:${Versions.constraint_layout}"
+
+    /** Unit tests dependencies */
+    const val junit = "junit:junit:${Versions.junit}"
+
+    /** Instrumentation tests dependencies */
+    const val androidx_junit = "androidx.test.ext:junit:${Versions.androidx_junit}"
+    const val androidx_espresso = "androidx.test.espresso:espresso-core:${Versions.androidx_espresso}"
+
+    /** Rx */
+    const val rxJava2 = "io.reactivex.rxjava2:rxjava:${Versions.rxJava2}"
+    const val rxJava2Android = "io.reactivex.rxjava2:rxandroid:${Versions.rxJava2Android}"
+    const val rxBinding = "com.jakewharton.rxbinding2:rxbinding:${Versions.rxBinding}"
+}
